3fddc70c69ba1c658c795af51c57c381f0fd10d7
Ratchasak Puttidechpaiboon <79717405+RatchaSun@users.noreply.github.com>
2021-10-17 11:59:37 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -2,7 +2,20 @@ function BMI() {
     var weight = document.getElementById("weight").value;
     var height = document.getElementById("height").value;
     var bmi = weight / (height / 100 * height / 100);
-    //weight / Math.pow(height / 100, 2);
     var bmio = (bmi.toFixed(2));
-    document.getElementById("result").innerHTML = bmio;
+    document.getElementById("result").innerHTML = "ค่า BMI ของคุณเท่ากับ " + bmio;
+}
+
+function newFunction() {
+    if (BMI >= 30) {
+        document.getElementById("result2").innerHTML = 'โรคอ้วนอันตราย';
+    } else if (BMI >= 25) {
+        document.getElementById("result2").innerHTML = 'โรคอ้วน';
+    } else if (BMI >= 23) {
+        document.getElementById("result2").innerHTML = 'นำหนักเกิน';
+    } else if (BMI >= 18.5) {
+        document.getElementById("result2").innerHTML = 'สมส่วน';
+    } else {
+        document.getElementById("result2").innerHTML = 'น้ำหนักต่ำกว่าเกณฑ์';
+    }
 }
